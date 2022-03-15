28b4668785404ed6ca23440eb5e83854ef44f4d4
Halie Dyer <halie.l.dyer@gmail.com>
2022-03-14 20:54:49 -0500

    Add project files.

---
+++ DogGo/SQL/SQLQuery2.sql
@@ -0,0 +1,17 @@
+﻿SELECT Id, 
+                                               Name,
+                                               Address,
+                                               Phone,
+                                               Email,
+                                               NeighborhoodId
+                                        FROM Owner
+SELECT w. Id, w.[Name], ImageUrl, n.[Name]
+FROM Walker w
+Left Join Neighborhood n on n.Id = w.NeighborhoodId
+
+Select *
+From Neighborhood
+
+SELECT o.Id, o.Name, Address, Phone, Email, NeighborhoodId, d.Name
+From Owner o
+Left Join Dog d on d.OwnerId = o.Id
